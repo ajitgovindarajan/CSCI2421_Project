aabc02d4ec44b9a87cd7a686717f06fcb39cdf7b
ajitgovindarajan <113131603+ajitgovindarajan@users.noreply.github.com>
2022-12-15 12:00:00 -0700

    Update Movies.cpp

--- Movies.cpp
+++ Movies.cpp
@@ -1,51 +1,125 @@
 //
 // Created by agovi on 11/21/2022.
 //
+
+#ifndef FINAL_PROJECT_MOVIES_H
+#define FINAL_PROJECT_MOVIES_H
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <Movies.h>
+#include <utility>
 using namespace std;
 
-void MovieBST::AddRecord(NodeMovie *leaf)
-{
+sturct Movies{
+    string name;
+    int year{};
+    int nominations= 0;
+    int rating = 0;
+    int duration;
+    string genre1;
+    string genre2;
+    string release;
+    string synopsis;
+    int metacritic = 0;
 
-    //No elements add the root and set the key
-    if (root == NULL)
-    {
-        // NodeActor *node = new NodeActor();
+    explicit Movies(vector<string>data){
+        try{
+            name = data[0];
+            year = stoi(data[1]);
+            // need to handle some of the blanks
+            nominations = stoi(data[2]);
+            //nominations = stoi(data[2]) ? data[2] = "-":0;
+            rating = stoi(data[3]);
+            duration = stof(data[4]);
+            genre1 = data[5];
+            genre2 = data[6];
+            release = data[7];
+            metacritic = stoi(data[8]);
+            synopsis = data[9];
+        }
+        catch (...){
+            cerr << "Something is not right\n"
+        }
 
-        //node->setKey(key);
-
-        root = leaf;
+        }
+        Movie(const string &movie_name) {
+            name = movie_name;
     }
-        // add a node
-    else
-        addRecord(leaf, root);
-}
-void MovieBST::addRecord(NodeMovie *node_to_add, NodeMovie *subtree_root)
-{
+        bool_operator < (const /*Picture*/movie &actor) const {
+            return (this-> name < actor.name);
+            out << "Name: " << Movies.name <<endl;
+            out << "Film: " << Movies.film <<endl;
+            out << "Synopsis: " << Movies.synopsis <<endl;
+            out << "Nominations: " << Movies.nominations <<endl;
+        }
 
-    if (subtree_root == nullptr)
-        subtree_root = node_to_add;
-    else if (node_to_add->key < subtree_root->key)
-        addRecord(node_to_add, subtree_root->leftNode);
-    else if (node_to_add->key > subtree_root->key)
-        addRecord(node_to_add, subtree_root->rightNode);
-}
-
-//Print Functions
-void MovieBST::Print(ostream &stream)
+        
+};
+/*
+class NodeMovie
 {
-    Print(stream, root);
-}
+    NodeMovie();
+    NodeMovie(string name, int year, int nominations, double rating, string genre1, string genre2,NodeMovie* left, NodeMovie* right){}
+    void setKey(string key){}
 
-void MovieBST::Print(ostream &stream, NodeMovie *subtree_root)
+public:
+    string key;
+    NodeMovie* leftNode;
+    NodeMovie* rightNode;
+};
+
+class MovieBST
 {
-    if(subtree_root!= nullptr)
-    {
-        Print(stream, subtree_root->leftNode);
-        stream << subtree_root->key << " ";
-        Print(stream, subtree_root->rightNode);
-    }
-}
+private:
+    NodeMovie* root;
+    void addRecord(NodeMovie *node_to_add,NodeMovie *subtree_root );
+    void Print(ostream& stream,NodeMovie* subtree_root);
+    void deleteRecord(NodeMovie *node_to_delete );
+
+    string name;
+    int year;
+    int nominations;
+    double rating;
+    string genre1;
+    string genre2;
+    string release;
+    int metacritic;
+    string synopsis;
+
+
+
+public:
+    MovieBST();
+    void setKey(string key){};
+    string key();
+    void AddRecord(NodeMovie* leaf); // function that will add a record
+    void ModifyRecord(string, int ); // function that will modify a record
+    void DeleteRecord(NodeMovie* leaf); // function that will delete a record
+    void Print(ostream& stream); // function that will export to file
+
+
+    //getters
+    string getName(){return name;};
+    int getNomination(){return nominations;};
+    double getRating(){return rating;};
+    string getGenre1(){return genre1;};
+    string getGenre2(){return genre2;};
+    string getRelease(){return release;};
+    int getMetacritic(){return metacritic;};
+    string getSynopsis(){return synopsis;};
+
+
+    //setters
+    void setName(string _name);
+    void setNomination(int _nominations);
+    void setRating(double _rating);
+    void setGenre1(string _genre1);
+    void setGenre2(string _genre2);
+    void setRelease(string _release);
+    void setMetacritic(int _metacritic);
+    void setSynopsis(string _synopsis);
+
+
+};
+#endif //FINAL_PROJECT_MOVIES_H
+*/
