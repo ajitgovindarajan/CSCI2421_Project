6d1369d159cfc633496c1c31953a66d5132ed91e
ajitgovindarajan <113131603+ajitgovindarajan@users.noreply.github.com>
2022-12-15 12:00:36 -0700

    Add files via upload

---
+++ Exceptions.h
@@ -0,0 +1,19 @@
+/*************************************************
+ * Author: Ajit Govindarajan
+ * Version: 11/16/2022
+ * The exceptions to the code are stored here
+ * **************************************************/
+
+#ifndef HW3_EXCEPTIONS_H
+#define HW3_EXCEPTIONS_H
+
+#include <exception>
+
+class UnderflowException:std::exception { };
+class IllegalArgumentException:std::exception { };
+class ArrayIndexOutOfBoundsException:std::exception { };
+class IteratorOutOfBoundsException:std::exception { };
+class IteratorMismatchException:std::exception { };
+class IteratorUninitializedException:std::exception { };
+
+#endif //HW3_EXCEPTIONS_H
