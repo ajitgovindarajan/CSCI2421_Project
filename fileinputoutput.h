1de2d401a7eb81fb55d67bcc845b6a5606f47eb1
ajitgovindarajan <113131603+ajitgovindarajan@users.noreply.github.com>
2022-12-16 19:42:24 -0700

    Update fileinputoutput.h

--- fileinputoutput.h
+++ fileinputoutput.h
@@ -1,6 +1,11 @@
-//
-// Created by agovi on 11/30/2022.
-//
+/*************************************************
+ * Version: 12/16/2022
+ * fileinputoutput.h provides all of the major methods of the implementation for the main
+ * data base extraction that will then go through manipulation into the final database
+ * that the user want to access
+ * This file will read the data from the file, read specifcally from actors or actresses data
+ * as well as exporting by printing as well
+ * **************************************************/
 #include <iostream>
 #include <string>
 #include "actorActress.h"
@@ -9,84 +14,80 @@
 #include <vector>
 #include <sstream>
 #include "Movies.h"
+#include<cstring>
+using namespace std;
+
 #ifndef FINAL_PROJECT_FILEINPUTOUTPUT_H
 #define FINAL_PROJECT_FILEINPUTOUTPUT_H
 
 
-void ReadFromFile(string & filepath,BinarySearchTree<actorActress> &bst){
-    fstream file(filepath);
+void tokenize(std::string const &str,const char delimit, std::vector<std::string> &out)
+{
+    std::stringstream ss(str);
 
+    std::string s;
+
+    while (std::getline(ss,s,delimit)){
+        out.push_back(s);
+    }
+}
+
+void ReadFromFile(string &filepath,BinarySearchTree<ActressActor> &bst){
+    /*
+    BinarySearchTree<ActressActor>ReadFromFile(std::string & filepath){
+        BinarySearchTree<actorActress> tempor;
+    }
+     */
+    fstream file(filepath);
+    BinarySearchTree<ActressActor> tempor;
     if (!file.is_open()){
         cerr<<"Input file not found" << filepath<<endl;
         exit(EXIT_FAILURE);
     }
-    // hint delimiter
-    // use tokenise function for delimit from the commas
-    string line
+    string line;
     while (getline(file,line)){
-        if (line = "Year,Award,Winner,Name,Film"){
+        if (line == "Year,Award,Winner,Name,Film"){
             continue;
-        }
-        vector <string> line_tokens;
-
+        vector<string> line_tokens;
         tokenize(line,',',line_tokens);
-        //cout << line << endl;
 
-        actorActress actorActress(line_tokens);
 
-        bst.insert(actorActress)
-
-        for (auto item: lineTokens){
-            cout << item << endl;
+            //ActressActor actress_actor(year,role,rank,name,title);
+        ActressActor actress_actor(line_tokens);
+            //tempor.Insert(actress_actor);
+        bst.Insert(actress_actor)
         }
         cout << "------------------------------------------------------------" << endl;
     }
 }
-void read_movies(string& pictures_filepath, BinarySearchTree<>()){
-    fstream file(pictures_filepath);
+void read_movies(string& pictures_filepath, BinarySearchTree<Movies> &bst){
+    std::fstream file(pictures_filepath);
 
     if (!file.is_open()){
         cerr<<"Input file not found" << pictures_filepath<<endl;
         exit(EXIT_FAILURE);
-    }
-    // hint delimier
-    // use tokenise function for delimit from the commas
-    string line
-    while (getline(file,line)){
-        // using a hack here
-        if (line.find("duration,genre1,genre2,release,metacritic,")=13){
-            continue;
-            //counter++;
+    } else {
+        string line;
+        while (getline(file,line)){
+            if (line.find("name,year,nominations,rating,duration,genre1,genre2,release,metacritic,synopsis")){
+                continue;
+            vector<string> line_tokens;
+            tokenize(line,',',line_tokens);
+
+            Movies movie(line_tokens);
+
+            bst.Insert(movie);
+
+            }
         }
-        vector <string> line_tokens;
-
-        tokenize(line,',',line_tokens);
-        //cout << line << endl;
-
-        Movies movies(line_tokens);
-
-        bst.insert(movies);
-
-        for (auto item: lineTokens){
-            cout << item << endl;
-        }
-        cout << "------------------------------------------------------------" << endl;
     }
-}
-
-BinarySearchTree<actorActress> ReadFromFile(string & filepath){
-    // need a binary search tree types
-    // use bst.h file from hw 3
-    BinarySearchTree<actorActress> tempor;
-
-    auto actors =
-    return tempor;
+    cout << "------------------------------------------------------------" << endl;
 }
 
 void printMovies_toFile(BinarySearchTree<Movies> &act){
     ofstream output("FinalMovie.csv");
-    output>> "Header of the original file";
-    auto pictora = act.get(act("Casablanca"));
+    output<< "name,year,nominations,rating,duration,genre1,genre2,release,metacritic,synopsis";
+    auto pictora = act.get(Movies("Casablanca"));
     cout << pictora << endl;
     output<<pictora.name<<","<<pictora.year << ",";
     output.close();
@@ -94,11 +95,10 @@ void printMovies_toFile(BinarySearchTree<Movies> &act){
 
 void printActors_toFile(BinarySearchTree<ActressActor> &act){
     ofstream output("FinalActors.csv");
-    output>> "Header of the original file";
+    output<< "Year,Award,Winner,Name,Film";
 
     auto acc = act.get(ActressActor(name));
-    output<<acc.year<<","<<acc.award<<","acc.film<<","<<acc.winner<<","<<acc.name;
-    for (auto &acc)
+    output<<acc.year<<","<<acc.award<<","<<acc.film<<","<<acc.winner<<","<<acc.name;
 //iterate over the full binary search tree
 // just call is recrusively
 // add a find and iterators
