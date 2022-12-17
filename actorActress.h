88133900432c73fe1198ad8b2476acd926af22a7
ajitgovindarajan <113131603+ajitgovindarajan@users.noreply.github.com>
2022-12-16 19:41:16 -0700

    Update actorActress.h

--- actorActress.h
+++ actorActress.h
@@ -1,12 +1,22 @@
-//
-// Created by agovi on 11/21/2022.
-//
+/*************************************************
+ * Version: 12/16/2022
+ * actorActress.h instantiates the object of the actorActress
+ * and associates the data sets with the values of neame, award won, the year it was won
+ * whether the actor/actress was a winner of an award and the film name that the award was won
+ * for.
+ * Once the object is instantiated, the fields for each category get filled in later for use of
+ * the other operations especially for the searching fields and the exportation of the records
+ * for the final database. This provides the frame work for when a record is added to ask for
+ * all the fields in the container that is an actor or a actress
+ * **************************************************/
 // need unique writeup- in the readme of how each student contributed in each way
 
 #ifndef FINAL_PROJECT_ACTORACTRESS_H
 #define FINAL_PROJECT_ACTORACTRESS_H
+
 #include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
 #include <utility>
 using namespace std;
@@ -14,15 +24,15 @@ using namespace std;
 
 struct ActressActor{
      unsigned int year;
-     string award;
+     std::string award;
      //int winner;
      short int winner;
      // could also do
      // bool winner;
-     string name;
-     string film;
+     std::string name;
+     std::string film;
 
-    explicit actorActress(vector<string> data){
+    explicit ActressActor(std::vector<std::string> data){
         year = stoi(data[0]);
         award = data[1];
         winner = stoi(data[2]);
@@ -30,54 +40,21 @@ struct ActressActor{
         film = data[4];
     }
 
-    actorActress
-    friend ostream operator << (ostream& out actorActress){
+    explicit ActressActor(std::string name) {
+        this->name = std::move(name);
+    }
+
+    friend std::ostream& operator << (ostream& out, ActressActor& actorActress){
         out << "Name; " << actorActress.name <<endl;
         out << "Film: " << actorActress.film <<endl;
         out << "Role: " << actorActress.award <<endl;
         out << "Year: " << actorActress.year <<endl;
-        out << "Winner: " << actorActress.winner <<endl;
+        out << "Rank: " << actorActress.winner <<endl;
 
         return out;
     }
-    bool_operator < (const actorActress &actor) const {
-        return (this-> name < actor.name);
+    bool operator < (const ActressActor &actor) const {
+        return (this->name < actor.name);
     }
 };
-
-/*
-class actorActress {
-public:
-    NodeActor();
-    NodeActor(string year, string award, string winner, string film, string name,NodeActor* left, NodeActor* right){}
-    void setKey(string key){}
-    string key;
-    NodeActor* leftNode;
-    NodeActor* rightNode;
-
-};
-
-class ActorBST
-{
-private:
-    NodeActor* root;
-    void addRecord(NodeActor *node_to_add,NodeActor *subtree_root );
-    void Print(ostream& stream,NodeActor* subtree_root);
-    void deleteRecord(NodeActor *node_to_delete, string key );
-    NodeActor* findNode(string key, NodeActor* subtree_root);
-
-public:
-    ActorBST();
-    void setKey(string key){};
-    string key();
-    void AddRecord(NodeActor* leaf); // function that will add a record
-    void ModifyRecord(string, int ); // function that will modify a record
-    void DeleteRecord(NodeActor* leaf); // function that will delete a record
-    void Print(ostream& stream); // function that will export to file
-    NodeActor* findNode();
-
-
-
-};
-*/
 #endif //FINAL_PROJECT_ACTORACTRESS_H
