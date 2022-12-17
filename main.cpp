7d2f6d49394fa9171e82c37957df45e84c5e3307
ajitgovindarajan <113131603+ajitgovindarajan@users.noreply.github.com>
2022-12-16 22:41:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 /*************************************************
  * Version: 12/16/2022
- * main,cpp is the driver code for this entire project. It will house the final main menu
- * that the user will use to decide what action will be done with the data base and which data
- * base will be affected.
+ * main,cpp is the driver code for this entire project. It will house the final
+ * main menu that the user will use to decide what action will be done with the
+ * data base and which data base will be affected.
  * **************************************************/
 
-#include "fileinputoutput.h"
-#include "Movies.h"
 #include "BST.h"
-#include "actorActress.h"
-#include "functions.h"
 #include "Exceptions.h"
+#include "Movies.h"
+#include "actorActress.h"
+#include "fileinputoutput.h"
+#include "functions.h"
 #include <cassert>
 #include <iostream>
 #include <string>
@@ -23,15 +23,14 @@ int main() {
     int databaseInput;
     fstream actDataBase;
     fstream pictureBase;
-    //std::string file;
     string movie_name;
     string actor_name;
 
-    BinarySearchTree<actorActress> actor_actress;
+    BinarySearchTree<ActressActor> actor_actress;
     BinarySearchTree<Movies> movie;
 
-    string actor_actress_file = "../input/actor_actress.csv";
-    string pictures_file = "../input/pictures.csv";
+    string actor_actress_file = "actor_actress.csv";
+    string pictures_file = "pictures.csv";
 
     ReadFromFile(actor_actress_file, actor_actress);
     read_movies(pictures_file, movie);
@@ -39,16 +38,17 @@ int main() {
     actDataBase.open(actor_actress.csv);
     pictureBase.open(pictures.csv);
 
-    cout<< "CSCI 2421 Final Project"<<endl;
+    cout << "CSCI 2421 Final Project" << endl;
 
-/*********************Main Menu*******************/
+    /*********************Main Menu*******************/
     cout << "Welcome to the Academy awards DataBase: " << endl;
     cout << "Please choose what you would like to do:" << endl;
     cout << "1. Movie database" << endl;
     cout << "2. Actor/Actress database" << endl;
-    cin << databaseInput;
+    cin >> databaseInput;
     if (databaseInput == 1) {
-        cout << "You have chosen " << databaseInput << " which is the Movie database" << endl;
+        cout << "You have chosen " << databaseInput
+             << " which is the Movie database" << endl;
         cout << "1. add a Record of the Database" << endl;
         cout << "2. Search/ Modify a Record of the Database" << endl;
         cout << "3. Search/Delete a record from the database" << endl;
@@ -56,7 +56,7 @@ int main() {
         cout << "5.Partial Search of the Data base" << endl;
         cout << "6. Export Database to .CSV file" << endl;
         cout << "7. Exit" << endl;
-        cin << mainMenuInput << endl;
+        cin >> mainMenuInput;
         if (mainMenuInput == 1) {
             cout << "Add a record" << endl;
             addMovie(movie);
@@ -76,8 +76,8 @@ int main() {
         } else if (mainMenuInput == 5) {
             cout << "Partial search of the movie database" << endl;
             cout << "Enter the name of the movie";
-            getline(cin,movie_name);
-            auto movieToSearch = PartialSearchMovie(movie,movie_name);
+            getline(cin, movie_name);
+            auto movieToSearch = PartialSearchMovie(movie, movie_name);
             cout << movieToSearch;
         } else if (mainMenuInput == 6) {
             cout << "Exporting the Movie Data base to a new csv file" << std::endl;
@@ -85,10 +85,11 @@ int main() {
         } else if (mainMenuInput == 7) {
             cout << "Exiting the program" << endl;
         } else {
-            cout << "Not valid selection" << end;
+            std::cout << "Not valid selection" << std::endl;
         }
     } else if (databaseInput == 2) {
-        cout << "You have chosen " << databaseInput << " which is the Actor/Actress database" << endl;
+        cout << "You have chosen " << databaseInput
+             << " which is the Actor/Actress database" << endl;
         cout << "1. add a Record of the Database" << endl;
         cout << "2. Search/ Modify a Record of the Database" << endl;
         cout << "3. Search/Delete a record from the database" << endl;
@@ -115,17 +116,18 @@ int main() {
             cout << "Print the Sorted Data Base " << endl;
             printActress_sorted(actor_actress);
         } else if (mainMenuInput == 5) {
-            cout << "The complete search of the actor/actress database" <<endl;
-            cout<< "Enter the Actor/Actress name: " << endl;
-            getline(cin,actor_name);
-            completeSearchActor(actor_actress,actor_name);
+            cout << "The complete search of the actor/actress database" << endl;
+            cout << "Enter the Actor/Actress name: " << endl;
+            getline(cin, actor_name);
+            completeSearchActor(actor_actress, actor_name);
         } else if (mainMenuInput == 6) {
-            cout << "Exporting the Actor/Actress database to a new csv file" << std::endl;
+            cout << "Exporting the Actor/Actress database to a new csv file"
+                 << std::endl;
             printActors_toFile(actor_actress);
         } else if (mainMenuInput == 7) {
             cout << "Exiting the program" << endl;
         } else {
-            cout << "Not valid selection" << end;
+            std::cout << "Not valid selection" << std::endl;
         }
     } else {
         std::cout << "Not a valid selection, try again" << std::endl;
